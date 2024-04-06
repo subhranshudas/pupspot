4db5e943d8d1d2abd9b98ba3c968203c5252e0f0
subhranshudas <subhranshu.opt@gmail.com>
2024-04-06 18:39:16 +0530

    add google tag from new conversion action

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="AW-16522584580" />
     </html>
   );
 }
