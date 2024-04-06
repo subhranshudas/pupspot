d197d0e4da2a905a353b7b9feeb1d6c8d191a5c3
subhranshudas <subhranshu.opt@gmail.com>
2024-04-06 19:13:14 +0530

    add link btn

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Link
+          href="/success"
+          className="px-12 py-4 bg-blue-800 text-white fixed top-2 right-2"
+        >
+          Go to Success
+        </Link>
+
+        {children}
+      </body>
       <GoogleAnalytics gaId="AW-16522584580" />
     </html>
   );
